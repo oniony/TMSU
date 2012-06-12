1379c0849142d2566ee7f755c23da16fd3aba122
oniony Paul Ruane <paul.ruane@oniony.com>
2012-06-12 18:12:02 +0100

    Added files omitted from previous check-in. Updated TODO.

---
+++ src/tmsu/commands/shared.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2011-2012 Paul Ruane.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package commands
+
+import (
+	"errors"
+	"strings"
+)
+
+
+func validateTagName(tagName string) error {
+	if strings.Index(tagName, ",") != -1 {
+		return errors.New("Tag names cannot contain commas.")
+	}
+
+	if strings.Index(tagName, "=") != -1 {
+		return errors.New("Tag names cannot contain '='.")
+	}
+
+	if strings.Index(tagName, " ") != -1 {
+		return errors.New("Tag names cannot contain spaces.")
+	}
+
+	if tagName[0] == '-' {
+	    return errors.New("Tag names cannot start with '-'.")
+    }
+
+    return nil
+}
---
+++ src/tmsu/vfs/mtable.go
@@ -0,0 +1,67 @@
+/*
+Copyright 2011-2012 Paul Ruane.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package vfs
+
+import (
+    "bytes"
+    "io/ioutil"
+    "os"
+    "os/exec"
+    "strings"
+    "tmsu/common"
+)
+
+type Mount struct {
+    DatabasePath string
+    MountPath    string
+}
+
+func GetMountTable() ([]Mount, error) {
+    //TODO change this to system calls
+
+    outputBytes, err := exec.Command("pgrep", "-f", "tmsu vfs").Output()
+    if err != nil {
+        return nil, err
+    }
+
+    pids := strings.Split(strings.Trim(string(outputBytes), "\n"), "\n")
+
+    mountTable := make([]Mount, len(pids))
+
+    for index, pid := range pids {
+        metaPath := "/proc/" + pid
+
+        workingDirectory, err := os.Readlink(metaPath + "/cwd")
+        if err != nil {
+            return nil, err
+        }
+
+        data, err := ioutil.ReadFile(metaPath + "/cmdline")
+        if err != nil {
+            return nil, err
+        }
+
+        tokens := bytes.Split(data, []byte{0})
+
+        databasePath := common.Join(workingDirectory, string(tokens[2]))
+        mountPath := common.Join(workingDirectory, string(tokens[3]))
+        mountTable[index] = Mount{databasePath, mountPath}
+    }
+
+    return mountTable, nil
+}

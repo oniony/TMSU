d3a3088d5e033d3872fe168e8de3ee443681938b
oniony Paul Ruane <paul.ruane@oniony.com>
2014-09-12 19:18:09 +0100

    Missed file.

---
+++ src/tmsu/storage/database/errors.go
@@ -0,0 +1,95 @@
+/*
+Copyright 2011-2014 Paul Ruane.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package database
+
+import (
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"tmsu/entities"
+)
+
+type DatabaseAccessError struct {
+	DatabasePath string
+	Reason       error
+}
+
+func (err DatabaseAccessError) Error() string {
+	return fmt.Sprintf("cannot access database at '%v': %v", err.DatabasePath, err.Reason)
+}
+
+type DatabaseTransactionError struct {
+	DatabasePath string
+	Reason       error
+}
+
+func (err DatabaseTransactionError) Error() string {
+	return fmt.Sprintf("database transaction error: %v", err.Reason)
+}
+
+type DatabaseQueryError struct {
+	DatabasePath string
+	Query        string
+	Reason       error
+}
+
+func (err DatabaseQueryError) Error() string {
+	return fmt.Sprintf("database query failed: %v", err.Reason)
+}
+
+type NoSuchFileError struct {
+	FileId entities.FileId
+}
+
+func (err NoSuchFileError) Error() string {
+	return fmt.Sprintf("no such file #%v", err.FileId)
+}
+
+type NoSuchValueError struct {
+	ValueId entities.ValueId
+}
+
+func (err NoSuchValueError) Error() string {
+	return fmt.Sprintf("no such value #%v", err.ValueId)
+}
+
+type NoSuchQueryError struct {
+	Query string
+}
+
+func (err NoSuchQueryError) Error() string {
+	return fmt.Sprintf("no such query '%v'", err.Query)
+}
+
+type NoSuchFileTagError struct {
+	FileId  entities.FileId
+	TagId   entities.TagId
+	ValueId entities.ValueId
+}
+
+func (err NoSuchFileTagError) Error() string {
+	return fmt.Sprintf("no such file-tag for file #%v, tag #%v and value #%v.", err.FileId, err.TagId, err.ValueId)
+}
+
+type NoSuchImplicationError struct {
+	TagId        entities.TagId
+	ImpliedTagId entities.TagId
+}
+
+func (err NoSuchImplicationError) Error() string {
+	return fmt.Sprintf("no such implication where tag #%v implies tag #%v", err.TagId, err.ImpliedTagId)
+}

493082be9e5c90bb1303aa1eee4848d6264da2bf
Paul Ruane <paul.ruane@oniony.com>
2015-01-09 21:32:34 +0000

    Don't try to create the database schema every time.
    TMSU now records the schema version in a 'version' table. If the schema
    version is the latest then the schema creation step is skipped. This
    improves performance and also paves way for TMSU to perform schema
    upgrades itself.

---
+++ src/tmsu/common/version.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2011-2015 Paul Ruane.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package common
+
+import (
+    "fmt"
+    "strings"
+    "strconv"
+)
+
+type Version struct {
+    Major uint
+    Minor uint
+    Revision uint
+}
+
+func ParseVersion(version string) Version {
+    parts := strings.Split(version, ".")
+
+    major, err := strconv.Atoi(parts[0])
+    if err != nil {
+        panic("invalid major version")
+    }
+
+    minor, err := strconv.Atoi(parts[1])
+    if err != nil {
+        panic("invalid minor version")
+    }
+
+    revision, err := strconv.Atoi(parts[2])
+    if err != nil {
+        panic("invalid revision version")
+    }
+
+    return Version{uint(major), uint(minor), uint(revision)}
+}
+
+func (version Version) String() string {
+    return fmt.Sprintf("%v.%v.%v", version.Major, version.Minor, version.Revision)
+}
+
+func (this Version) LessThan(that Version) bool {
+    return this.Major < that.Major || this.Minor < that.Minor || this.Revision < that.Revision
+}
+
+func (this Version) GreaterThan(that Version) bool {
+    return this.Major > that.Major || this.Minor > that.Minor || this.Revision > that.Revision
+}

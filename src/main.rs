d0178699c1934cf08503d528f53939dafd79bbaf
Paul Ruane <paul.ruane@oniony.com>
2017-12-01 20:36:27 +0000

    Playing with Clap.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 extern crate clap;
 
-use clap::{App, SubCommand};
+use clap::{App, AppSettings, Arg, SubCommand};
 
 fn main() {
     let matches = App::new("TMSU").version("1.0.0")
+                                  .template("{bin}\n\n{subcommands}\n\nGlobal options:\n\n{flags}\n\n{after-help}")
+                                  .after_help("Specify subcommand name for detailed help on a particular subcommand, e.g. tmsu help files")
+                                  .arg(Arg::with_name("verbose").short("v")
+                                                                .long("verbose")
+                                                                .help("Show verbose messages"))
+                                  .subcommand(SubCommand::with_name("config").about("Views or amends database sesttings"))
                                   .subcommand(SubCommand::with_name("files").about("Lists files with particular tags"))
                                   .get_matches();
 }

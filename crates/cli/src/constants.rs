8ae702338240f8067163bd228eb9279348ab3b2c
Paul Ruane <paul.ruane@oniony.com>
2025-09-20 02:11:11 +0100

    Fixed environment variable name

--- crates/cli/src/constants.rs
+++ crates/cli/src/constants.rs
@@ -16,5 +16,5 @@
 pub const APPLICATION_NAME: &str = "tmsu";
 pub const APPLICATION_DIRECTORY: &str = ".tmsu";
 pub const DEFAULT_DATABASE_NAME: &str = "db";
-pub const DATABASE_ENV_VAR: &str = "TMSU_DATABASE";
+pub const DATABASE_ENV_VAR: &str = "TMSU_DB";
 
